5d92d5eacd6018f05488e28cde3f7a220b3dca92
saumil <shah.saumil66@gmail.com>
2024-05-31 20:41:51 -0700

    Starting the dev environment

---
+++ base_setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Install git, vim, and tmux
+
+# Install google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Install VS Code
+sudo snap install --classic code # or code-insiders
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
